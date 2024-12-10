252a4ba7cdd70321fc7be1614e203925d85a11ad
atowey_uchi <atowey@uchicago.edu>
2024-12-10 11:27:03 -0500

    fixing exports

--- R/data_objects.R
+++ R/data_objects.R
@@ -1,44 +1,44 @@
-#' #' All Countries Data
-#' #'
-#' #' A dataset containing the names of all countries.
-#' #'
-#' #' @format A character vector with country names.
-#' #' @name all_countries
-#' #' @export
-#' #load("data/all_countries.RData")
-#' load(system.file("data", "all_countries.RData", package = "visiblespectrum"))
+#' All Countries Data
 #'
+#' A dataset containing the names of all countries.
 #'
-#' #' All Indicators Data
-#' #'
-#' #' A dataset containing the names of all indicators.
-#' #'
-#' #' @format A character vector with indicator names.
-#' #' @name all_indicators
-#' #' @export
-#' #load("data/all_indicators.RData")
-#' load(system.file("data", "all_indicators.RData", package = "visiblespectrum"))
+#' @format A character vector with country names.
+#' @name all_countries
+#' @export
+#load("data/all_countries.RData")
+load(system.file("data", "all_countries.RData", package = "visiblespectrum"))
+
+
+#' All Indicators Data
 #'
-#' #' Indicators to Indicator Codes Data
-#' #'
-#' #' A dataset containing the names of all indicators and their respective codes
-#' #' used to create URLs on the website.
-#' #'
-#' #' @format A data frame with columns for indicator names and their corresponding codes.
-#' #' @name indicators_df
-#' #' @export
-#' #load("data/indicators_df.RData")
-#' load(system.file("data", "indicators_df.RData", package = "visiblespectrum"))
+#' A dataset containing the names of all indicators.
 #'
+#' @format A character vector with indicator names.
+#' @name all_indicators
+#' @export
+#load("data/all_indicators.RData")
+load(system.file("data", "all_indicators.RData", package = "visiblespectrum"))
+
+#' Indicators to Indicator Codes Data
 #'
-#' #' Country Max Levels Data
-#' #'
-#' #' A dataset containing the maximum levels for each country.
-#' #'
-#' #' @format A data frame with columns for country names and their corresponding max levels.
-#' #' @name country_max_levels_df
-#' #' @export
-#' #load("data/country_max_levels.RData")
-#' load(system.file("data", "country_max_levels.RData", package = "visiblespectrum"))
+#' A dataset containing the names of all indicators and their respective codes
+#' used to create URLs on the website.
 #'
+#' @format A data frame with columns for indicator names and their corresponding codes.
+#' @name indicators_df
+#' @export
+#load("data/indicators_df.RData")
+load(system.file("data", "indicators_df.RData", package = "visiblespectrum"))
+
+
+#' Country Max Levels Data
 #'
+#' A dataset containing the maximum levels for each country.
+#'
+#' @format A data frame with columns for country names and their corresponding max levels.
+#' @name country_max_levels_df
+#' @export
+#load("data/country_max_levels.RData")
+load(system.file("data", "country_max_levels.RData", package = "visiblespectrum"))
+
+
